0b4c348977800b6142d45bc2e3c77e4eb2626885
Struan Donald <struan@exo.org.uk>
2018-08-01 16:06:22 +0100

    [Open311] add customer reference from update to problem
    If an Open311 update has a customer_reference property then add that to
    the metadata for the problem and display that instead of the external_id
    for OCC.

--- perllib/Open311/GetServiceRequestUpdates.pm
+++ perllib/Open311/GetServiceRequestUpdates.pm
@@ -115,6 +115,7 @@ sub update_comments {
                 my $state = $open311->map_state( $request->{status} );
                 my $old_state = $p->state;
                 my $external_status_code = $request->{external_status_code} || '';
+                my $customer_reference = $request->{customer_reference} || '';
                 my $old_external_status_code = $p->get_extra_metadata('external_status_code') || '';
                 my $comment = $self->schema->resultset('Comment')->new(
                     {
@@ -142,6 +143,12 @@ sub update_comments {
                     $comment->set_extra_metadata(external_status_code => $external_status_code);
                     $p->set_extra_metadata(external_status_code => $external_status_code);
                 }
+
+                # if the customer reference to display in the report metadata is
+                # not the same as the external_id
+                if ( $customer_reference ) {
+                    $p->set_extra_metadata( customer_reference => $customer_reference );
+                )
 
                 $open311->add_media($request->{media_url}, $comment)
                     if $request->{media_url};
