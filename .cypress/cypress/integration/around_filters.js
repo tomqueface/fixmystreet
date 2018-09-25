063070fea9904c6a3be0595ea5517c73d497b8e0
Struan Donald <struan@exo.org.uk>
2018-09-25 12:16:42 +0100

    update around page tests to also test push state

--- .cypress/cypress/integration/around_filters.js
+++ .cypress/cypress/integration/around_filters.js
@@ -1,63 +1,98 @@
-describe('Filtering around page', function() {
+describe('Around page filtering and push state', function() {
     before(function(){
         cy.visit('/reports/Borsetshire');
         cy.contains('Borsetshire');
     });
 
     it('allows me to filter by report status', function() {
-        cy.contains('1 to 14 of 14');
+        cy.server();
+        cy.route('/reports/Borsetshire\?ajax*').as('update-results');
+        cy.contains('1 to 15 of 15');
         cy.get('.multi-select-button:first').click();
         cy.get('#status_0').click();
         cy.get('#status_1').click();
+        cy.wait('@update-results');
         cy.contains('1 to 3 of 3');
-        cy.contains('Lights out in tunnel');
+        cy.contains('Full litter bins');
         cy.url().should('include', 'status=closed');
         cy.get('#status_2').click();
+        cy.wait('@update-results');
         cy.contains('1 to 6 of 6');
-        cy.contains('Loose drain cover');
+        cy.contains('Street light not working');
         cy.url().should('include', 'status=closed%2Cfixed');
-        cy.get('#status_preset_0').click();
-        cy.get('#status_0').should('be.checked');
-        cy.get('#status_1').should('be.checked');
         cy.get('#status_2').should('be.checked');
-        cy.contains('1 to 20 of 20');
+        cy.go('back');
+        cy.wait('@update-results');
+        cy.contains('1 to 3 of 3');
+        cy.should('not.contain', 'Street light not working');
+        cy.url().should('not.include', 'fixed');
+        cy.get('#status_2').should('not.be.checked');
+      /* TODO: not sure this should work
+        cy.go('forward');
+        cy.wait('@update-results');
+        cy.contains('1 to 6 of 6');
+        cy.contains('Light not going off');
+        cy.url().should('include', 'status=closed%2Cfixed');
+        cy.get('#status_2').should('be.selected');
+        */
     });
 
     it('allows me to filter by report category', function() {
+        cy.server();
+        cy.route('/reports/Borsetshire\?ajax*').as('update-results');
         cy.visit('/reports/Borsetshire');
-        cy.contains('1 to 14 of 14');
+        cy.contains('1 to 15 of 15');
         cy.get('.multi-select-button:eq(1)').click();
-        cy.get('input[value=Graffiti]').click();
-        cy.contains('1 to 14 of 14');
-        cy.contains('Graffiti on the bridge');
-        cy.get('input[value=Graffiti]').click();
+        cy.get('input[value=Potholes]').click();
+        cy.wait('@update-results');
+        cy.url().should('include', 'filter_category=Potholes');
+        cy.contains('1 to 4 of 4');
+        cy.contains('Large pothole');
+        cy.get('input[value=Potholes]').click();
         cy.get('input[value=Other]').click();
-        cy.contains('1 to 2 of 2');
-        cy.contains('Full litter bins');
-        cy.should('not.contain', 'Graffiti on the bridge');
+        cy.wait('@update-results');
+        cy.url().should('include', 'filter_category=Other');
+        cy.contains('1 to 4 of 4');
+        cy.contains('Loose drain cover');
+        cy.should('not.contain', 'Large pothole');
+      /* TODO: not sure this works
+        cy.go('back');
+        cy.wait('@update-results');
+        cy.url().should('include', 'filter_category=Potholes');
+        cy.contains('1 to 3 of 3');
+        cy.contains('Large pothole');
+        */
     });
-
 
     it('allows me to sort', function() {
         cy.server();
         cy.route('/reports/Borsetshire\?ajax*').as('update-results');
         cy.visit('/reports/Borsetshire');
-        cy.contains('1 to 14 of 14');
+        cy.contains('1 to 15 of 15');
         cy.get('#sort').select('created-desc');
         cy.url().should('include', 'sort=created-desc');
         cy.wait('@update-results');
-        cy.get('.item-list__heading:first').contains('Large pothole');
+        cy.get('.item-list__heading:first').contains('Flytipping on country lane');
         cy.get('#sort').select('created-asc');
         cy.wait('@update-results');
-        cy.get('.item-list__heading:first').contains('Full litter bins');
+        cy.get('.item-list__heading:first').contains('Offensive graffiti');
         cy.get('#sort').select('updated-asc');
         cy.wait('@update-results');
-        cy.get('.item-list__heading:first').contains('Full litter bins');
+        cy.get('.item-list__heading:first').contains('Faulty light');
         cy.get('#sort').select('updated-desc');
         cy.wait('@update-results');
-        cy.get('.item-list__heading:first').contains('Full litter bins');
+        cy.get('.item-list__heading:first').contains('Pothole in cycle lane');
         cy.get('#sort').select('comments-desc');
-        cy.wait('@update-results:first');
-        cy.get('.item-list__heading').contains('Full litter bins');
+        cy.wait('@update-results');
+        cy.get('.item-list__heading').contains('Offensive graffiti');
     });
+
+    it('lets me go from around -> report and back', function() {
+        cy.server();
+        cy.route('/report/*').as('show-report');
+        cy.visit('/reports/Borsetshire');
+        cy.get('.item-list--reports__item:first a').click();
+        cy.wait('@show-report');
+    });
+
 });
